376898503b8c38fb25aa5766e56d297ce57847d2
Preston Hodsman <phodsman@yahoo.com>
2021-10-01 12:12:07 -0700

    Completed the functionality for importing and cleaning the data. moved these functions to separate files in the utils folder. began adding comments.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Cryptocurrency-Arbitrage-Opportunity-Analyzer
 Analyzes opportunities to profit from arbitrage trading by comparing and zooming in on areas of interest in CSV data sets of different cryptocurrency markets. Uses Jupyter Lab and outputs visual analysis and summary statistics.
+
---
+++ utils/data_clean.py
@@ -0,0 +1,18 @@
+
+# This function cleans a data frame that has first been imported using the cyryptoprices_data_import function for analysis and returns it. 
+# The data is expected to have a Close column, representing the closing prices for each Timestamp. 
+# The Close column will be checked for null values, which will be removed. 
+# The Close column will be checked for dollars signs, which will be removed. 
+# The Close column will be converted to float values. 
+# Duplicate lines will be removed.
+
+import pandas as pd
+
+def data_clean(pricedata):
+    if pricedata.isnull().sum().sum() > 0:
+        pricedata = pricedata.dropna()
+    pricedata.loc[:, "Close"] = pricedata.loc[:, "Close"].str.replace("$", "")
+    pricedata.loc[:, "Close"] = pricedata.loc[:, "Close"].astype("float")
+    if pricedata.duplicated().sum() > 0:
+        pricedata = bitstamp.drop_duplicates()
+    return pricedata
