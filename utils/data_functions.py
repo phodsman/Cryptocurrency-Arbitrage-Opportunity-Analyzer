7d37af60287f515f823a09989d629c66be6973b0
Preston Hodsman <phodsman@yahoo.com>
2021-10-05 10:16:41 -0700

    Added code to filter arbitrage spread data by trades with spread over zero, to divide those spreads by the price of the higher priced market, and to further filter by trades with have a spread over the 1 percent cost of transaction.

--- utils/data_functions.py
+++ utils/data_functions.py
@@ -11,7 +11,7 @@
 import pandas as pd
 
 def data_clean(pricedata):
-    if pricedata.isnull().sum().sum() > 0:
+    if pricedata.isnull().sum() > 0:
         pricedata = pricedata.dropna()
     pricedata.loc[:, "Close"] = pricedata.loc[:, "Close"].str.replace("$", "")
     pricedata.loc[:, "Close"] = pricedata.loc[:, "Close"].astype("float")
@@ -38,3 +38,7 @@ def plot_compare_for_date_range(bitstamp_sliced, coinbase_sliced, start_date, en
 def arbitrage_spread_for_date_range(bitstamp_sliced, coinbase_sliced, start_date, end_date):
     arbitrage_spread_for_range = bitstamp_sliced.loc[start_date : end_date] - coinbase_sliced.loc[start_date : end_date]
     return arbitrage_spread_for_range
+
+# def arbitrage_spread_positive_for_date_range(bitstamp_sliced, coinbase_sliced, start_date, end_date):
+#     arbitrage_spread_positive_for_range = bitstamp_sliced.loc[start_date : end_date] - coinbase_sliced.loc[start_date : end_date]
+#     return arbitrage_spread_positive_for_range
